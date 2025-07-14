3d8d8232fa78be1b915e3c911cf9438a8579428b
DESKTOP-B5HI9JS\hp <heshamhamdan51@gmail.com>
2025-07-14 22:56:28 +0300

    Implement video upload functionality with video player integration; update pubspec.yaml and GeneratedPluginRegistrant.swift for new dependencies.

--- lib/screens/upload_video.dart
+++ lib/screens/upload_video.dart
@@ -1,7 +1,51 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class UploadVideo extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+
+class UploadVideo extends StatefulWidget {
   const UploadVideo({super.key});
+
+  @override
+  State<UploadVideo> createState() => _UploadVideoState();
+}
+
+class _UploadVideoState extends State<UploadVideo> {
+  XFile? videoSelected;
+  VideoPlayerController? controller;
+
+  /// upload video
+  Future<void> uploadVideo() async {
+    final pickedVideo = await ImagePicker().pickVideo(
+      source: ImageSource.gallery,
+    );
+
+    if (pickedVideo != null) {
+      controller = VideoPlayerController.file(File(pickedVideo.path));
+    }
+
+    setState(() {
+      videoSelected = pickedVideo;
+    });
+
+    controller!.play();
+
+    controller!.setLooping(true);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    if (controller != null) {
+      controller!.dispose();
+    }
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,22 +64,36 @@ class UploadVideo extends StatelessWidget {
                 borderRadius: BorderRadius.circular(6),
                 color: Colors.grey.withValues(alpha: 0.6),
               ),
+
+              child: controller != null && controller!.value.isInitialized
+                  ? ClipRRect(
+                      borderRadius: BorderRadiusGeometry.circular(12),
+                      child: AspectRatio(
+                        aspectRatio: controller!.value.aspectRatio,
+                        child: VideoPlayer(controller!),
+                      ),
+                    )
+                  : null,
             ),
 
             SizedBox(height: 40),
-            Container(
-              height: 40,
-              width: 180,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(16),
-                color: Colors.green.withValues(alpha: 0.5),
-              ),
-              child: Center(
-                child: Text(
-                  'upload video',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+            // button for chosse video
+            GestureDetector(
+              onTap: uploadVideo,
+              child: Container(
+                height: 40,
+                width: 180,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.green.withValues(alpha: 0.5),
+                ),
+                child: Center(
+                  child: Text(
+                    'upload video',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
               ),
