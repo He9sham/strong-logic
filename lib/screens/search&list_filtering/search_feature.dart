49500a848a5d7f2bf68be80f8fbef5d7e3f4bf1c
DESKTOP-B5HI9JS\hp <heshamhamdan51@gmail.com>
2025-07-31 18:22:26 +0300

    Enhance SearchFeature screen by adding product list and updating UI elements; include search field with icons and adjust layout for improved user experience.

--- lib/screens/search&list_filtering/search_feature.dart
+++ lib/screens/search&list_filtering/search_feature.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchFeature extends StatefulWidget {
@@ -8,32 +9,102 @@ class SearchFeature extends StatefulWidget {
 }
 
 class _SearchFeatureState extends State<SearchFeature> {
+  List<Map> products = [
+    {
+      "image":
+          "https://plus.unsplash.com/premium_photo-1664392147011-2a720f214e01?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "title": "Brown Women Bag",
+      "price": 70,
+    },
+    {
+      "image":
+          "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "title": "White Watch 40",
+      "price": 130,
+    },
+    {
+      "image":
+          "https://images.unsplash.com/photo-1541643600914-78b084683601?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "title": "Men Chanel Perfume",
+      "price": 2000,
+    },
+    {
+      "image":
+          "https://images.unsplash.com/photo-1572635196237-14b3f281503f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8cHJvZHVjdHxlbnwwfHwwfHx8MA%3D%3D",
+      "title": "Black Modern Glasses",
+      "price": 1400,
+    },
+    {
+      "image":
+          "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2R1Y3R8ZW58MHx8MHx8fDA%3D",
+      "title": "Men Nike Shoes",
+      "price": 150,
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple.shade900,
+      backgroundColor: Colors.pink.shade900,
 
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           children: [
-            SizedBox(height: 150),
-
+            SizedBox(height: 120),
+            // Text field for search
             SizedBox(
-              height: 40,
+              height: 45,
               child: TextField(
                 cursorColor: Colors.black,
                 decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                  prefixIcon: Icon(CupertinoIcons.search),
+                  suffixIcon: Icon(CupertinoIcons.xmark),
+                  contentPadding: EdgeInsets.symmetric(horizontal: 12),
                   hintText: 'Search',
+                  helperStyle: TextStyle(color: Colors.black),
                   enabledBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(8),
                   ),
+
                   focusedBorder: OutlineInputBorder(),
                   filled: true,
                   fillColor: Colors.grey.shade300,
                 ),
               ),
+            ),
+
+            SizedBox(height: 50),
+
+            Column(
+              children: List.generate(5, (index) {
+                final product = products[index];
+                return ListTile(
+                  contentPadding: EdgeInsets.symmetric(vertical: 10),
+                  leading: ClipRRect(
+                    borderRadius: BorderRadiusGeometry.circular(8),
+                    child: Image.network(
+                      product['image'],
+                      fit: BoxFit.cover,
+                      height: 80,
+                      width: 80,
+                    ),
+                  ),
+                  title: Text(
+                    product['title'],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  trailing: Text(
+                    '${product['price']}\$',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                );
+              }),
             ),
           ],
         ),
