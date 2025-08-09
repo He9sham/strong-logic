07f98d1f7b7382b5ca01c6bb323c6932f606b74d
DESKTOP-B5HI9JS\hp <heshamhamdan51@gmail.com>
2025-08-09 13:56:26 +0300

    Implement form validation in FormValidation screen; add email and password fields with validation logic, and display request status upon login attempt.

--- lib/screens/form_validation/form_validation.dart
+++ lib/screens/form_validation/form_validation.dart
@@ -8,6 +8,10 @@ class FormValidation extends StatefulWidget {
 }
 
 class _FormValidationState extends State<FormValidation> {
+  final formkey = GlobalKey<FormState>();
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  String request = '';
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -17,76 +21,109 @@ class _FormValidationState extends State<FormValidation> {
 
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            children: [
-              SizedBox(height: 150),
-              // from email
-              TextFormField(
-                cursorColor: Colors.white,
-                style: TextStyle(
-                  color: Colors.white,
-                ), // This sets the text color
-                decoration: InputDecoration(
-                  hintText: 'Email address',
-                  hintStyle: TextStyle(color: Colors.grey.shade500),
-                  prefixIcon: Icon(
-                    Icons.email_outlined,
-                    color: Colors.grey.shade500,
-                  ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey.shade700),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-              SizedBox(height: 15),
-              // from password
-              TextFormField(
-                cursorColor: Colors.white,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  hintText: 'Password',
-                  hintStyle: TextStyle(color: Colors.grey.shade500),
-                  prefixIcon: Icon(
-                    Icons.lock_outline,
-                    color: Colors.grey.shade500,
-                  ),
-                  suffixIcon: Icon(Icons.remove_red_eye),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey.shade700),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                ),
-              ),
-
-              SizedBox(height: 30),
-              // Login Container
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  width: double.infinity,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Login',
-                      style: TextStyle(fontWeight: FontWeight.bold),
+          child: Form(
+            key: formkey,
+            child: Column(
+              children: [
+                SizedBox(height: 150),
+                // from email
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Fill the email';
+                    }
+                    if (!value!.endsWith('@gmail.com')) {
+                      return 'write Email in right format';
+                    }
+                  },
+                  controller: email,
+                  cursorColor: Colors.white,
+                  style: TextStyle(
+                    color: Colors.white,
+                  ), // This sets the text color
+                  decoration: InputDecoration(
+                    hintText: 'Email address',
+                    hintStyle: TextStyle(color: Colors.grey.shade500),
+                    prefixIcon: Icon(
+                      Icons.email_outlined,
+                      color: Colors.grey.shade500,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade700),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10),
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 15),
+                // from password
+                TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Fill the password';
+                    }
+                  },
+                  controller: password,
+                  cursorColor: Colors.white,
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.grey.shade500),
+                    prefixIcon: Icon(
+                      Icons.lock_outline,
+                      color: Colors.grey.shade500,
+                    ),
+                    suffixIcon: Icon(Icons.remove_red_eye),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey.shade700),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 30),
+                // Login Container
+                GestureDetector(
+                  onTap: () {
+                    if (formkey.currentState!.validate()) {
+                      setState(() {
+                        request = 'request';
+                      });
+                    } else {
+                      return;
+                    }
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 15),
+
+                Text(
+                  'Request : $request',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
       ),
